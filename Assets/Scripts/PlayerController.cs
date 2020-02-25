934015605e96bfe5eb84dfcbf7345fbd397b9935
E. Harris <56174820+fcr-harris@users.noreply.github.com>
2020-02-25 12:24:07 -0800

    Powerup and Countdown day 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public bool hasPowerup = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +20,18 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed *forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.CompareTag("Powerup")){
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup){
+            Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerup);
+        }
     }
 }
