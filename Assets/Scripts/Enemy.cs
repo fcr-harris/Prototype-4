db0a88d9ce91743527a28db1d5d998eed8e080b1
E. Harris <56174820+fcr-harris@users.noreply.github.com>
2020-02-27 12:11:26 -0800

    For-Loops for Waves

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,13 +10,18 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y < -10){
+            Destroy(gameObject);
+        }
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
     }
